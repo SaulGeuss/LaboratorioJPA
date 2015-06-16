575dcaa1ee4c1ded5b7903648dd268a6a5e89690
adiseño.2015 <adiseo.2015@192.168.56.1>
2015-06-16 11:58:20 -0600

    Se creo la clase ConexionBD, en donde se encuentra la unica conexion a la base de datos

---
+++ src/ModeloDeMVC/ConexionBD.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModeloDeMVC;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.*;
+
+/**
+ *
+ * @author adiseño.2015
+ */
+public class ConexionBD {
+    
+    
+}
