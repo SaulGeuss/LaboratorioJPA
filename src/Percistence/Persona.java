d2430492ab0b67b239e61b3ef6e2f80fdcb7f629
adiseño.2015 <adiseo.2015@192.168.56.1>
2015-06-16 10:50:53 -0600

    Inicializacion del proyecto, creacion de la base de datos y la clase persona y su percistence

---
+++ src/Percistence/Persona.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Percistence;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ *
+ * @author adiseño.2015
+ */
+
+@Entity                  //Entity en una tabla, le estoy diciendo a java que lo trate como una tabla
+@NamedQueries({
+    @NamedQuery(name="persona.findNombreLike", query=" select u from persona u " + "where u.nombre LIKE :nombre")
+})
+
+
+public class Persona {
+    
+    @Id  //indica que la fila de abajo la trate como un id
+    private int id;
+    
+    private String nombre;
+    private String apellido;
+    private Boolean conectado;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Boolean getConectado() {
+        return conectado;
+    }
+
+    public void setConectado(Boolean conectado) {
+        this.conectado = conectado;
+    }
+    
+    @Override
+    public String toString(){
+        return "Persona: " + nombre + " ,Apellido: " + apellido + " ,Conectado: " + conectado; 
+    }
+    
+    
+}
